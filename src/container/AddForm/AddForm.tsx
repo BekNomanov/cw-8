47cf73389b20956b1f78a0682cbb8a04a6e239ea
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-02-10 12:38:14 +0600

    сделал форму для отправки пост запросов на api

--- src/container/AddForm/AddForm.tsx
+++ src/container/AddForm/AddForm.tsx
@@ -1,7 +1,82 @@
-const AddForm = () => {
+import React, { FormEvent, useState } from 'react';
+import axiosApi from '../../axiosApi';
+import {Props} from '../../type';
+
+const AddForm: React.FC = () => {
+  const [formData, setFormData] = useState<Props>({
+    category: '',
+    author: '',
+    text: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axiosApi.post('/quotes.json', formData);
+      console.log('Post added successfully:', response.data);
+    } catch (error) {
+      console.error('Error adding post:', error);
+    }
+  };
+
   return (
-    <div>
-    <h1>efoeofeof</h1>
+    <div className='container-lg'>
+      <h2>Добавить пост</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group mb-3">
+          <label htmlFor="category" className="input-group-text">Категория</label>
+          <select
+            id="category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            className="form-select"
+            required
+          >
+            <option value="">Выберите категорию</option>
+            <option value="star-wars">Star Wars</option>
+            <option value="famous-people">Famous people</option>
+            <option value="saying">Saying</option>
+            <option value="humour">Humour</option>
+            <option value="motivational">Motivational</option>
+          </select>
+        </div>
+        <div className="input-group mb-3">
+          <label htmlFor="author" className="input-group-text">Автор</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={formData.author}
+            onChange={handleChange}
+            required
+            className="form-control"
+            aria-label="Автор"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="text" className="input-group-text">Текст</label>
+          <textarea
+            id="text"
+            name="text"
+            value={formData.text}
+            onChange={handleChange}
+            required
+            className="form-control"
+            aria-label="Текст"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">Создать пост</button>
+      </form>
     </div>
   );
 };
